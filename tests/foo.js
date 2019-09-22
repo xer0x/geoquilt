a383598cffbe515897dcabdb1f0086716db07f85
Drew Miller <drew666@gmail.com>
2019-09-21 23:12:45 -0700

    Test out the Geocoding api

--- tests/foo.js
+++ tests/foo.js
@@ -1,6 +1,6 @@
 var test = require('tape');
 
-test('timing test', function (t) {
+test.skip('timing test', function (t) {
     t.plan(2);
 
     t.equal(typeof Date.now, 'function');
--- tests/geocoder.js
+++ tests/geocoder.js
@@ -4,17 +4,57 @@ var geocoder = require('../lib/geocoder')
 require('dotenv').config()
 
 const api_key = process.env.GOOGLE_GEOCODER_KEY
-const {geocode} = geocoder(api_key)
 
-test('Basic geocoder works', async function (assert) {
+test('Geocoder has valid api key', async function (assert) {
   assert.plan(1)
 
   try {
-    const response = await geocode("the statue of liberty")
-    console.log(response)
-    assert.equal(response, 'special value from google')
+    const {geocode} = geocoder(api_key)
+    const response = await geocode('the statue of liberty')
+    assert.notEqual(response, undefined);
   } catch (e) {
     assert.fail(e)
+  }
+
+})
+
+test('Returns multiple possible matches', async function (assert) {
+  assert.plan(2)
+
+  try {
+    const {geocode} = geocoder(api_key)
+    const response = await geocode('booo, naniamo, alaska, canada')
+    assert.notEqual(response, undefined)
+    assert.equal(response.length, 2, 'expect 2 possible matches for Naniamo and Alaska')
+  } catch (e) {
+    assert.fail(e)
+  }
+
+})
+
+test('should throw error on empty input', async function (assert) {
+  assert.plan(1)
+
+  try {
+    const {geocode} = geocoder(api_key)
+    const response = await geocode('')
+    assert.fail('should have thrown an error')
+  } catch (e) {
+    assert.equals(e.message, 'Response status code is 400', 'expect a 400 error')
+  }
+
+})
+
+test('should find the statue of liberty', async function (assert) {
+  assert.plan(2)
+
+  try {
+    const {geocode} = geocoder(api_key)
+    const response = await geocode('the statue of liberty')
+    assert.ok(response)
+    assert.equal(response.length, 1, 'should on know 1 location for the statue')
+  } catch (e) {
+    assert.ifError(e)
   }
 
 })
