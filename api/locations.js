1b94a06404d73883e7b83d6d1220ce0dd9698085
Drew Miller <drew666@gmail.com>
2019-09-22 15:20:02 -0700

    Sketching out api endpoint

--- api/locations.js
+++ api/locations.js
@@ -1,7 +1,38 @@
+const {findClosestLocations} = require('../lib')
+
+/**
+ * # POST /api/location
+ *
+ * Expects the req.body to be JSON with the following format:
+ *
+ * {
+ *  "locations": [ { "name": "Statue of Liberty" }, { "name": "Miami, FL" }, ... ]
+ * }
+ *
+ * The response body will be in the following format:
+ *
+ * HTTP 200 Success:
+ * {
+ *   "locations": [
+ *     {"name": "Statue of Liberty", "": ""},
+ *     ...
+ *   ],
+ *   "geocode_errors": []
+ * }
+ *
+ * HTTP 400 Failures:
+ * {
+ *   "error": ""
+ * }
+ */
 module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies
-  })
+
+  try {
+    const {locations} = json.parse(req.body)
+    const result = findClosestLocations(locations)
+    res.status(200).json(result)
+  } catch (e) {
+    res.status(400).json({ error: e.message })
+  }
+
 }
