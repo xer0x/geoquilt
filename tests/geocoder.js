cfd7d254d321d01aee8d8862a238c1777dad4c6e
Drew Miller <drew666@gmail.com>
2019-09-21 22:16:31 -0700

    fix: update dependencies to actually include node-geocoder

--- tests/geocoder.js
+++ tests/geocoder.js
@@ -4,13 +4,13 @@ var geocoder = require('../lib/geocoder')
 require('dotenv').config()
 
 const api_key = process.env.GOOGLE_GEOCODER_KEY
-const {lookup} = geocoder(api_key)
+const {geocode} = geocoder(api_key)
 
 test('Basic geocoder works', async function (assert) {
   assert.plan(1)
 
   try {
-    const response = await lookup("the statue of liberty")
+    const response = await geocode("the statue of liberty")
     console.log(response)
     assert.equal(response, 'special value from google')
   } catch (e) {
